4f9db2a4c738d97349db24aab394e65c93bcd3c7
ysh0305 <s1hung@ucsd.edu>
2026-02-03 11:14:26 -0800

    Remove submodules to relink cleanly

--- docs/galyleo/archive/test2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#files_to_transfer='    /wnt/dev /proc/sys,/tmp,/usr/portage, /var/tmp'
-
-IFS=', '
-read -r -a files <<< "$(echo ${files_to_transfer})"
-for file in "${files[@]}"; do
-  echo $file
-done
-unset IFS
